9e835f9bd04ba2cce8a859b26fe8ecc10dba5771
kartarake <kar.prahveen@gmail.com>
2024-09-08 10:48:19 +0530

    Adding long_description

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="karlib",
     version="0.0.1",
     description="Kar's library of utilities",
+    long_description="This is an open source python library of useful utilities that simplify everyday coding task.",
     author="kartarake",
     author_email="kar.prahveen@gmail.com",
     url="https://github.com/kartarake/KarLib.git",
